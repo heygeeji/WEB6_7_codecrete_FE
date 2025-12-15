70adfc4a95cea91d7b9b2c381f60e36fd5e365f6
varYeon <xjkooy100@gmail.com>
2025-12-15 12:07:47 +0900

    style(mate) : 구인 글 목록 퍼블리싱

--- src/components/concert-mate/list/MateListPostList.tsx
+++ src/components/concert-mate/list/MateListPostList.tsx
@@ -1,8 +1,46 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import MateListCard from "./MateListCard";
+
 export default function MateListPostList() {
   return (
     <section className="mx-auto w-full max-w-400">
-      <h1>MateListPostList</h1>
-      <h2>MateListPostList</h2>
+      <div className="flex flex-col gap-6 py-12">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <MateListCard key={index} />
+        ))}
+      </div>
+      <Pagination className="py-12">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#" isActive>
+              2
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </section>
   );
 }
